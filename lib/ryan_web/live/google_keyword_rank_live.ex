e6fe5b2557b4a879e2aa680ff296844d0bdd0cdf
ryan senn <ryan.senn.au@gmail.com>
2020-04-18 17:04:21 +1000

    num=100

--- lib/ryan_web/live/google_keyword_rank_live.ex
+++ lib/ryan_web/live/google_keyword_rank_live.ex
@@ -8,11 +8,9 @@ defmodule RyanWeb.GoogleKeywordRankLive do
   end
 
   def handle_event("submit", %{"domain" => domain, "keywords" => keywords}, socket) do
-    url = "https://www.google.com/search?q=#{URI.encode(keywords)}"
+    url = "https://www.google.com/search?q=#{URI.encode(keywords)}&num=100"
 
-    {:noreply, socket}
-
-    case fetch([], url) do
+    case fetch(url) do
       {:ok, results} ->
         {:noreply,
          assign(socket, error: nil, results: results, domain: domain, keywords: keywords)}
@@ -22,26 +20,16 @@ defmodule RyanWeb.GoogleKeywordRankLive do
     end
   end
 
-  defp fetch(acc, url) do
-    case HTTPoison.get(url) do
+  defp fetch(url) do
+    case HTTPoison.get(url, [], timeout: 10_000, recv_timeout: 10_000) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        links =
+        results =
           body
           |> Floki.parse_document!()
           |> Floki.find("a")
+          |> Enum.reduce([], fn node, acc -> acc ++ transform_node(node) end)
 
-        results = Enum.reduce(links, [], fn node, acc -> acc ++ transform_node(node) end)
-
-        next = "https://www.google.com" <> find_next(links)
-
-        acc = acc ++ results
-
-        if Enum.count(acc) < 100 do
-          Process.sleep(Enum.random(100..1000))
-          fetch(acc, next)
-        else
-          {:ok, acc}
-        end
+        {:ok, results}
 
       {:ok, %HTTPoison.Response{status_code: 302}} ->
         {:error, "Google rate limit"}
@@ -63,21 +51,4 @@ defmodule RyanWeb.GoogleKeywordRankLive do
   end
 
   defp transform_node(_), do: []
-
-  defp find_next([]), do: nil
-
-  defp find_next(links) do
-    [head | tail] = links
-    find_next(head, tail)
-  end
-
-  defp find_next({"a", attributes, _}, links) do
-    if Enum.any?(attributes, fn attribute -> attribute == {"aria-label", "Next page"} end) do
-      {_, value} = Enum.find(attributes, fn {key, _} -> key == "href" end)
-
-      value
-    else
-      find_next(links)
-    end
-  end
 end
